9cf6f6a7da934d9ca7f2e0319886f11304e60d0f
samuelayano <samuelayano7@gmail.com>
2022-12-30 16:40:27 +0100

    Swagger docs setup

--- src/__init__.py
+++ src/__init__.py
@@ -1,12 +1,12 @@
 import os
-from flask import Flask, jsonify
-from src.url import url
+from flask import Flask, jsonify, redirect
 from src.auth import auth
 from src.bookmarks import bookmarks
-from src.database import db
+from src.database import db, Bookmark
 from src.constants.http_status_codes import *
 from flask_jwt_extended import JWTManager
-
+from flasgger import Swagger, swag_from
+from src.config.swagger import template, swagger_config
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
@@ -16,19 +16,38 @@ def create_app(test_config=None):
             SECRET_KEY=os.environ.get("SECRET_KEY"),
             SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI"),
             SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get("SQLALCHEMY_TRACK_MODIFICATIONS"),
-            JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY")
+            JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY"),
+
+            SWAGGER ={
+                'title':"Bookmark API",
+                'uiversion':3
+            }
         )
     else:
         app.config.from_mapping(test_config)
 
     db.init_app(app)
 
+    # configure JWTManager
     JWTManager(app)
+
+    #configure Swagger
+    Swagger(app, template=template, config=swagger_config)
+
     # Registering blueprints
     app.register_blueprint(auth)
     app.register_blueprint(bookmarks)
-    app.register_blueprint(url)
    
+    # Route to handle short_url 
+    @app.get('/<short_url>')
+    @swag_from('./short_url.yaml')
+    def redirect_to_url(short_url):
+        bookmark = Bookmark.query.filter_by(short_url=short_url).first_or_404()
+        
+        if bookmark:
+            bookmark.visits = bookmark.visits +  1
+            db.session.commit()
+            return redirect(bookmark.url)
     # Routes to handle error exceptions
     @app.errorhandler(HTTP_404_NOT_FOUND)
     def handle_404(e):
---
+++ src/config/swagger.py
@@ -0,0 +1,46 @@
+template = {
+  "swagger": "2.0",
+  "info": {
+    "title": "Bookmark API",
+    "description": "Bookmark API for my data",
+    "contact": {
+      "responsibleOrganization": "ME",
+      "responsibleDeveloper": "Me",
+      "email": "samuelayano7@gmail.com",
+      "url": "https://www.linkedin.com/in/samuel-ayano-1336bb247/",
+    },
+    "termsOfService": "http://me.com/terms",
+    "version": "1.0"
+  },
+  "host": "localhost:5000",  # overrides localhost:500
+  "basePath": "/api/v1",  # base bash for blueprint registration
+  "schemes": [
+    "http",
+    "https"
+  ],
+   "securityDefinitions": {
+        "Bearer": {
+            "type": "apiKey",
+            "name": "Authorization",
+            "in": "header",
+            "description": "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\""
+        }
+    },
+  "operationId": "getmyData"
+}
+
+swagger_config = {
+    "headers": [
+    ],
+    "specs": [
+        {
+            "endpoint": 'apispec',
+            "route": '/apispec.json',
+            "rule_filter": lambda rule: True,  # all in
+            "model_filter": lambda tag: True,  # all in
+        }
+    ],
+    "static_url_path": "/flasgger_static",
+    "swagger_ui": True,
+    "specs_route": "/"
+}
