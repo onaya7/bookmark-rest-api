891edf53ea42de4cd7cf768735276eeb08e03558
samuel <samuelayano7@gmail.com>
2023-04-15 10:14:14 +0100

    Docs:made changed to swagger docs

--- src/config/swagger.py
+++ src/config/swagger.py
@@ -12,10 +12,9 @@ template = {
     "termsOfService": "http://me.com/terms",
     "version": "1.0"
   },
-  "host": "https://bookmark-api-3nfy.onrender.com",  # overrides localhost:500
+  "host": "bookmark-api-3nfy.onrender.com",  # overrides localhost:500
   "basePath": "/api/v1",  # base bash for blueprint registration
   "schemes": [
-    "http",
     "https"
   ],
    "securityDefinitions": {
