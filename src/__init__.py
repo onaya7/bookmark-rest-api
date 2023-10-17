9990b5013d0ea3585db4d6981ab8bd253bc4faf2
samuel <samuelayano7@gmail.com>
2023-10-18 00:21:24 +0100

    doc: modified .env

--- src/__init__.py
+++ src/__init__.py
@@ -25,7 +25,7 @@ def create_app(test_config=None):
         
         app.config.from_mapping(
             SECRET_KEY=os.environ.get("SECRET_KEY"),
-            SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI"),
+            SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI"),
             SQLALCHEMY_TRACK_MODIFICATIONS=os.environ.get("SQLALCHEMY_TRACK_MODIFICATIONS"),
             JWT_SECRET_KEY=os.environ.get("JWT_SECRET_KEY"),
             WTF_CSRF_SECRET_KEY=os.environ.get("WTF_CSRF_SECRET_KEY"),
